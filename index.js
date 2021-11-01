890fd1d249825a46808453bebda66586f1f6fd6f
Wasanta <kwan_u_day@hotmail.com>
2021-11-01 19:08:04 -0400

    commit tree 7be798478fac30e0643b2c6bcf8edc1f7ad0276b

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = ""
+currentUser= "Kwan"
+let welcomeMessage = "Welcome to Flatbook, ";
+welcomeMessage = "Welcome to Flatbook, "+ currentUser+"!";
+let  excitedWelcomeMessage ="WELCOME TO FLATBOOK, "
+excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+ currentUser.toUpperCase()+"!";
+let shortGreeting = "Welcome, "
+shortGreeting = "Welcome, "+ currentUser.charAt(0)+"!";
